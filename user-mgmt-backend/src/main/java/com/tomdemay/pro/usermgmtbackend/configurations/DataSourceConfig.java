76f0ac344b6163410f9c1694e46f7099f34e8a70
Tom DeMay <57812487+tomdemay@users.noreply.github.com>
2023-07-21 19:39:13 -0400

    more tweaks

--- user-mgmt-backend/src/main/java/com/tomdemay/pro/usermgmtbackend/configurations/DataSourceConfig.java
+++ user-mgmt-backend/src/main/java/com/tomdemay/pro/usermgmtbackend/configurations/DataSourceConfig.java
@@ -10,26 +10,19 @@ import javax.sql.DataSource;
 @Configuration
 public class DataSourceConfig {
 
-    @Value("${PORTFOLIO_RDS_ENDPOINT}")
-    private String rds_endpoint;
-
-    @Value("${PORTFOLIO_RDS_PROTOCOL}")
-    private String rds_protocol;
-
     @Value("${PORTFOLIO_RDS_BACKEND_USERNAME}")
     private String rds_username;
 
     @Value("${PORTFOLIO_RDS_BACKEND_PASSWORD}")
     private String rds_password;
 
-    private String getConnectionString() {
-        return String.format("jdbc:%s/%s", rds_protocol, rds_endpoint);
-    }
+    @Value("${PORTFOLIO_RDS_ENDPOINT}")
+    private String rds_endpoint;
 
     @Bean
     public DataSource dataSource() {
         return DataSourceBuilder.create()
-                .url(getConnectionString())
+                .url("jdbc:"+rds_endpoint)
                 .username(rds_username)
                 .password(rds_password)
                 .build();
